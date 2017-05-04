plan44/p44vdc

commit 2eaa0123989a869523daa7df1eff103dcf33d20d

Property access: added mechanism to allow a preparation step (possibly needing I/O) before a property can be accessed.
In particular, single devices might have deviceProperties that need a fetch cycle to the actual device's API to get the current value.

vdc_common/dsaddressable.cpp
@@ -156,13 +156,10 @@ ErrorPtr DsAddressable::handleMethod(VdcApiRequestPtr aRequest, const string &aM
     ApiValuePtr query;
     if (Error::isOK(respErr = checkParam(aParams, "query", query))) {
       // now read
-      ApiValuePtr result = aRequest->newApiValue();
-      respErr = accessProperty(access_read, query, result, VDC_API_DOMAIN, PropertyDescriptorPtr());
-      if (Error::isOK(respErr)) {
-        // send back property result
-        aRequest->sendResult(result);
-        respErr.reset(); // make sure we don't send an extra ErrorOK
-      }
+      accessProperty(
+        access_read, query, VDC_API_DOMAIN, PropertyDescriptorPtr(),
+        boost::bind(&DsAddressable::propertyAccessed, this, aRequest, _1, _2)
+      );
     }
   }
   else if (aMethod=="setProperty") {
@@ -175,11 +172,10 @@ ErrorPtr DsAddressable::handleMethod(VdcApiRequestPtr aRequest, const string &aM
       if (o) {
         preload = o->boolValue();
       }
-      respErr = accessProperty(preload ? access_write_preload : access_write, value, ApiValuePtr(), VDC_API_DOMAIN, PropertyDescriptorPtr());
-      if (Error::isOK(respErr)) {
-        // send back OK if write was successful
-        respErr = Error::ok();
-      }
+      accessProperty(
+        preload ? access_write_preload : access_write, value, VDC_API_DOMAIN, PropertyDescriptorPtr(),
+        boost::bind(&DsAddressable::propertyAccessed, this, aRequest, _1, _2)
+      );
     }
   }
   else if (aMethod=="genericRequest") {
@@ -204,9 +200,24 @@ ErrorPtr DsAddressable::handleMethod(VdcApiRequestPtr aRequest, const string &aM
 }
 
 
+void DsAddressable::propertyAccessed(VdcApiRequestPtr aRequest, ApiValuePtr aResultObject, ErrorPtr aError)
+{
+  if (Error::isOK(aError)) {
+    // read - send back property result
+    aRequest->sendResult(aResultObject);
+  }
+  else {
+    // just return status or error
+    aRequest->sendStatus(aError);
+  }
+}
+
+
+
+
 void DsAddressable::methodCompleted(VdcApiRequestPtr aRequest, ErrorPtr aError)
 {
-  // devices re-collected, return status
+  // method completed, return status
   aRequest->sendStatus(aError);
 }
 
@@ -220,23 +231,12 @@ bool DsAddressable::pushNotification(ApiValuePtr aPropertyQuery, ApiValuePtr aEv
     ApiValuePtr value;
     if (aPropertyQuery) {
       // a property change is to be notified
-      // - get the value by issuing the query
-      value = aPropertyQuery->newValue(apivalue_object);
-      err = accessProperty(access_read, aPropertyQuery, value, aDomain, PropertyDescriptorPtr());
+      accessProperty(
+        access_read, aPropertyQuery, aDomain, PropertyDescriptorPtr(),
+        boost::bind(&DsAddressable::pushPropertyReady, this, aEvents, _1, _2)
+      );
     }
-    if (Error::isOK(err)) {
-      // - send pushNotification
-      ApiValuePtr pushParams;
-      if (value) {
-        pushParams = value->newValue(apivalue_object);
-        pushParams->add("changedproperties", value);
-      }
-      if (aEvents) {
-        if (!pushParams) pushParams = aEvents->newValue(apivalue_object);
-        pushParams->add("deviceevents", aEvents);
-      }
-      return sendRequest("pushNotification", pushParams);
-    }
+    return true; // although not yet sent, assume push will be ok
   }
   else {
     if (isPublicDS()) {
@@ -244,8 +244,30 @@ bool DsAddressable::pushNotification(ApiValuePtr aPropertyQuery, ApiValuePtr aEv
       ALOG(LOG_WARNING, "pushNotification suppressed - is not yet announced");
     }
   }
-  return false;
+  return false; // push not possible now
 }
+
+
+void DsAddressable::pushPropertyReady(ApiValuePtr aEvents, ApiValuePtr aResultObject, ErrorPtr aError)
+{
+  if (Error::isOK(aError)) {
+    // - send pushNotification
+    ApiValuePtr pushParams;
+    if (aResultObject) {
+      pushParams = aResultObject->newValue(apivalue_object);
+      pushParams->add("changedproperties", aResultObject);
+    }
+    if (aEvents) {
+      if (!pushParams) pushParams = aEvents->newValue(apivalue_object);
+      pushParams->add("deviceevents", aEvents);
+    }
+    sendRequest("pushNotification", pushParams);
+  }
+  else {
+    ALOG(LOG_WARNING, "push failed because to-be-pushed property could not be accessed: %s", aError->description().c_str());
+  }
+}
+
 
 
 

vdc_common/device.cpp
@@ -592,8 +592,8 @@ void Device::handleNotification(const string &aMethod, ApiValuePtr aParams)
         // - channelStates
         ApiValuePtr propValue = ch->newObject();
         propValue->add("channelStates", ch);
-        // now access the property
-        err = accessProperty(apply_now ? access_write : access_write_preload, propValue, ApiValuePtr(), VDC_API_DOMAIN, PropertyDescriptorPtr());
+        // now access the property for write
+        accessProperty(apply_now ? access_write : access_write_preload, propValue, VDC_API_DOMAIN, PropertyDescriptorPtr(), NULL); // no callback
       }
     }
     if (!Error::isOK(err)) {

vdc_common/dsaddressable.hpp
@@ -341,6 +341,8 @@ namespace p44 {
 
   private:
 
+    void propertyAccessed(VdcApiRequestPtr aRequest, ApiValuePtr aResultObject, ErrorPtr aError);
+    void pushPropertyReady(ApiValuePtr aEvents, ApiValuePtr aResultObject, ErrorPtr aError);
     void presenceResultHandler(bool aIsPresent);
 
   };

vdc_common/propertycontainer.cpp
@@ -34,10 +34,62 @@
 using namespace p44;
 
 
-// MARK: ===== property access API
+
+void PropertyContainer::accessProperty(PropertyAccessMode aMode, ApiValuePtr aQueryObject, int aDomain, PropertyDescriptorPtr aParentDescriptor, PropertyAccessCB aAccessCompleteCB)
+{
+  // create a list for possibly needed preparations
+  PropertyPrepListPtr prepList = PropertyPrepListPtr(new PropertyPrepList);
+  // first attempt to access
+  // - create result object of same API type as query
+  ApiValuePtr result;
+  if (aMode==access_read) result = aQueryObject->newObject();
+  ErrorPtr err = accessPropertyInternal(aMode, aQueryObject, result, aDomain, aParentDescriptor, prepList);
+  if (prepList->empty()) {
+    // no need for preparation, immediately call back
+    if (aAccessCompleteCB) aAccessCompleteCB(result, err);
+    // done
+    return;
+  }
+  // need preparation
+  prepareNext(prepList, aMode, aQueryObject, aDomain, aParentDescriptor, aAccessCompleteCB, ErrorPtr());
+}
 
 
-ErrorPtr PropertyContainer::accessProperty(PropertyAccessMode aMode, ApiValuePtr aQueryObject, ApiValuePtr aResultObject, int aDomain, PropertyDescriptorPtr aParentDescriptor)
+void PropertyContainer::prepareNext(PropertyPrepListPtr aPrepList, PropertyAccessMode aMode, ApiValuePtr aQueryObject, int aDomain, PropertyDescriptorPtr aParentDescriptor, PropertyAccessCB aAccessCompleteCB, ErrorPtr aError)
+{
+  if (!Error::isOK(aError)) {
+    LOG(LOG_WARNING, "- prepraration of property failed with error: %s", aError->description().c_str());
+  }
+  if (aPrepList->empty()) {
+    // all prepared, access again
+    // - create result object of same API type as query
+    ApiValuePtr result;
+    if (aMode==access_read) result = aQueryObject->newObject();
+    ErrorPtr err = accessPropertyInternal(aMode, aQueryObject, result, aDomain, aParentDescriptor, PropertyPrepListPtr());
+    // call back
+    if (aAccessCompleteCB) aAccessCompleteCB(result, err);
+    // done
+    return;
+  }
+  // prepare next item on the list
+  PropertyPrep prep = aPrepList->front();
+  aPrepList->pop_front();
+  prep.target->prepareAccess(aMode, prep.propertyDescriptor,
+    boost::bind(&PropertyContainer::prepareNext, this, aPrepList, aMode, aQueryObject, aDomain, aParentDescriptor, aAccessCompleteCB, _1)
+  );
+}
+
+
+
+void PropertyContainer::prepareAccess(PropertyAccessMode aMode, PropertyDescriptorPtr aPropertyDescriptor, StatusCB aPreparedCB)
+{
+  // nothing to prepare in base class
+  if (aPreparedCB) aPreparedCB(ErrorPtr());
+}
+
+
+
+ErrorPtr PropertyContainer::accessPropertyInternal(PropertyAccessMode aMode, ApiValuePtr aQueryObject, ApiValuePtr aResultObject, int aDomain, PropertyDescriptorPtr aParentDescriptor, PropertyPrepListPtr aPreparationList)
 {
   ErrorPtr err;
   FOCUSLOG("\naccessProperty: entered with query = %s", aQueryObject->description().c_str());
@@ -90,86 +142,98 @@ ErrorPtr PropertyContainer::accessProperty(PropertyAccessMode aMode, ApiValuePtr
         if (propDesc) {
           foundone = true; // found at least one descriptor for this query element
           FOCUSLOG("  - processing descriptor '%s' (%s), fieldKey=%zu, objectKey=%lu", propDesc->name(), propDesc->isStructured() ? "structured" : "scalar", propDesc->fieldKey(), propDesc->objectKey());
-          // actually access by descriptor
-          if (aMode==access_write && propDesc->isDeletable() && queryValue->isNull()) {
-            // assigning NULL means deleting (possibly entire substructure)
-            if (!accessField(access_delete, queryValue, propDesc)) { // delete
-              err = Error::err<VdcApiError>(403, "Cannot delete '%s'", propDesc->name());
-            }
-          }
-          else if (propDesc->isStructured()) {
-            ApiValuePtr subQuery;
-            // property is a container. Now check the value
-            if (queryValue->isType(apivalue_object)) {
-              subQuery = queryValue; // query specifies next level, just use it
-            }
-            else if (queryName!="*" && (!wildcard || propDesc->isWildcardAddressable())) {
-              // don't recurse deeper when query name is "*" or property is not wildcard-adressable
-              // special case is "*" as leaf in query - only recurse if it is not present
-              // - autocreate subquery
-              subQuery = queryValue->newValue(apivalue_object);
-              subQuery->add("", queryValue->newValue(apivalue_null));
-            }
-            if (subQuery) {
-              // addressed property is a container by itself -> recurse
-              // - get the PropertyContainer
-              int containerDomain = aDomain; // default to same, but getContainer may modify it
-              PropertyDescriptorPtr containerPropDesc = propDesc;
-              PropertyContainerPtr container = getContainer(containerPropDesc, containerDomain);
-              if (container) {
-                FOCUSLOG("  - container for '%s' is 0x%p", propDesc->name(), container.get());
-                FOCUSLOG("    >>>> RECURSING into accessProperty()");
-                if (container!=this) {
-                  // switching to another C++ object -> starting at root level in that object
-                  containerPropDesc->rootOfObject = true;
-                }
-                if (aMode==access_read) {
-                  // read needs a result object
-                  ApiValuePtr resultValue = queryValue->newValue(apivalue_object);
-                  err = container->accessProperty(aMode, subQuery, resultValue, containerDomain, containerPropDesc);
-                  if (Error::isOK(err)) {
-                    // add to result with actual name (from descriptor)
-                    FOCUSLOG("\n  <<<< RETURNED from accessProperty() recursion");
-                    FOCUSLOG("  - accessProperty of container for '%s' returns %s", propDesc->name(), resultValue->description().c_str());
-                    aResultObject->add(propDesc->name(), resultValue);
-                  }
-                }
-                else {
-                  // for write, just pass the query value
-                  err = container->accessProperty(aMode, subQuery, ApiValuePtr(), containerDomain, containerPropDesc);
-                  FOCUSLOG("    <<<< RETURNED from accessProperty() recursion", propDesc->name(), container.get());
-                }
-                if ((aMode!=access_read) && Error::isOK(err)) {
-                  // give this container a chance to post-process write access
-                  err = writtenProperty(aMode, propDesc, aDomain, container);
-                }
-                // 404 errors are collected, but dont abort the query
-                if (Error::isError(err, VdcApiError::domain(), 404)) {
-                  if (!errorMsg.empty()) errorMsg += "; ";
-                  errorMsg += string_format("Error(s) accessing subproperties of '%s' : { %s }", queryName.c_str(), err->description().c_str());
-                  err.reset(); // forget the error on this level
-                }
-              }
+          // check if property needs preparation before being accessed
+          if (aPreparationList && propDesc->needsPreparation(aMode)) {
+            // collecting list of to-be-prepared properties, and this one needs prep -> add it
+            aPreparationList->push_back(PropertyPrep(this, propDesc));
+            FOCUSLOG("- property '%s' needs preparation -> added to preparation list (%d items now)", propDesc->name(), aPreparationList->size());
+            if (aMode==access_read) {
+              // read access: return NULL for to-be-prepared properties
+              aResultObject->add(propDesc->name(), queryValue->newNull());
             }
           }
           else {
-            // addressed (and known by descriptor!) property is a simple value field -> access it
-            if (aMode==access_read) {
-              // read access: create a new apiValue and have it filled
-              ApiValuePtr fieldValue = queryValue->newValue(propDesc->type()); // create a value of correct type to get filled
-              bool accessOk = accessField(aMode, fieldValue, propDesc); // read
-              // for read, not getting an OK from accessField means: property does not exist (even if known per descriptor),
-              // so it will not be added to the result
-              if (accessOk) {
-                // add to result with actual name (from descriptor)
-                aResultObject->add(propDesc->name(), fieldValue);
+            // actually access by descriptor
+            if (aMode==access_write && propDesc->isDeletable() && queryValue->isNull()) {
+              // assigning NULL means deleting (possibly entire substructure)
+              if (!accessField(access_delete, queryValue, propDesc)) { // delete
+                err = Error::err<VdcApiError>(403, "Cannot delete '%s'", propDesc->name());
               }
-              FOCUSLOG("    - accessField for '%s' returns %s", propDesc->name(), fieldValue->description().c_str());
+            }
+            else if (propDesc->isStructured()) {
+              ApiValuePtr subQuery;
+              // property is a container. Now check the value
+              if (queryValue->isType(apivalue_object)) {
+                subQuery = queryValue; // query specifies next level, just use it
+              }
+              else if (queryName!="*" && (!wildcard || propDesc->isWildcardAddressable())) {
+                // don't recurse deeper when query name is "*" or property is not wildcard-addressable
+                // special case is "*" as leaf in query - only recurse if it is not present
+                // - autocreate subquery
+                subQuery = queryValue->newValue(apivalue_object);
+                subQuery->add("", queryValue->newValue(apivalue_null));
+              }
+              if (subQuery) {
+                // addressed property is a container by itself -> recurse
+                // - get the PropertyContainer
+                int containerDomain = aDomain; // default to same, but getContainer may modify it
+                PropertyDescriptorPtr containerPropDesc = propDesc;
+                PropertyContainerPtr container = getContainer(containerPropDesc, containerDomain);
+                if (container) {
+                  FOCUSLOG("  - container for '%s' is 0x%p", propDesc->name(), container.get());
+                  FOCUSLOG("    >>>> RECURSING into accessProperty()");
+                  if (container!=this) {
+                    // switching to another C++ object -> starting at root level in that object
+                    containerPropDesc->rootOfObject = true;
+                  }
+                  if (aMode==access_read) {
+                    // read needs a result object
+                    ApiValuePtr resultValue = queryValue->newValue(apivalue_object);
+                    err = container->accessPropertyInternal(aMode, subQuery, resultValue, containerDomain, containerPropDesc, aPreparationList);
+                    if (Error::isOK(err)) {
+                      // add to result with actual name (from descriptor)
+                      FOCUSLOG("\n  <<<< RETURNED from accessProperty() recursion");
+                      FOCUSLOG("  - accessProperty of container for '%s' returns %s", propDesc->name(), resultValue->description().c_str());
+                      aResultObject->add(propDesc->name(), resultValue);
+                    }
+                  }
+                  else {
+                    // for write, just pass the query value
+                    err = container->accessPropertyInternal(aMode, subQuery, ApiValuePtr(), containerDomain, containerPropDesc, aPreparationList);
+                    FOCUSLOG("    <<<< RETURNED from accessProperty() recursion", propDesc->name(), container.get());
+                  }
+                  if ((aMode!=access_read) && Error::isOK(err)) {
+                    // give this container a chance to post-process write access
+                    err = writtenProperty(aMode, propDesc, aDomain, container);
+                  }
+                  // 404 errors are collected, but dont abort the query
+                  if (Error::isError(err, VdcApiError::domain(), 404)) {
+                    if (!errorMsg.empty()) errorMsg += "; ";
+                    errorMsg += string_format("Error(s) accessing subproperties of '%s' : { %s }", queryName.c_str(), err->description().c_str());
+                    err.reset(); // forget the error on this level
+                  }
+                }
+              }
             }
             else {
-              // write access: just pass the value
-              if (!accessField(aMode, queryValue, propDesc)) { // write
-                err = Error::err<VdcApiError>(403, "Write access to '%s' denied", propDesc->name());
+              // addressed (and known by descriptor!) property is a simple value field -> access it
+              if (aMode==access_read) {
+                // read access: create a new apiValue and have it filled
+                ApiValuePtr fieldValue = queryValue->newValue(propDesc->type()); // create a value of correct type to get filled
+                bool accessOk = accessField(aMode, fieldValue, propDesc); // read
+                // for read, not getting an OK from accessField means: property does not exist (even if known per descriptor),
+                // so it will not be added to the result
+                if (accessOk) {
+                  // add to result with actual name (from descriptor)
+                  aResultObject->add(propDesc->name(), fieldValue);
+                }
+                FOCUSLOG("    - accessField for '%s' returns %s", propDesc->name(), fieldValue->description().c_str());
+              }
+              else {
+                // write access: just pass the value
+                if (!accessField(aMode, queryValue, propDesc)) { // write
+                  err = Error::err<VdcApiError>(403, "Write access to '%s' denied", propDesc->name());
+                }
               }
             }
           }
@@ -415,8 +479,8 @@ bool PropertyContainer::readPropsFromCSV(int aDomain, bool aOnlyExplicitlyOverri
         break;
       }
     }
-    // now access that property
-    ErrorPtr err = accessProperty(access_write, property, ApiValuePtr(), aDomain, PropertyDescriptorPtr());
+    // now access that property (note: preparation is not checked so properties must be writable without preparation)
+    ErrorPtr err = accessPropertyInternal(access_write, property, ApiValuePtr(), aDomain, PropertyDescriptorPtr(), PropertyPrepListPtr());
     if (!Error::isOK(err)) {
       LOG(LOG_ERR, "%s:%d - error writing property '%s': %s", aTextSourceName, aLineNo, f.c_str(), err->description().c_str());
     }

vdc_common/propertycontainer.hpp
@@ -56,7 +56,9 @@ namespace p44 {
     proptype_mask = 0x3F,
     propflag_container = 0x80, ///< is a container
     propflag_nowildcard = 0x40, ///< don't recurse into this container when addressed via wildcard
-    propflag_deletable = 0x100 ///< can be deleted by writing NULL to it
+    propflag_deletable = 0x100, ///< can be deleted by writing NULL to it
+    propflag_needsreadprep = 0x200, ///< needs to be prepared before reading
+    propflag_needswriteprep = 0x400, ///< needs to be prepared before writing
   } PropertyFlags;
 
 
@@ -88,6 +90,8 @@ namespace p44 {
     virtual bool isArrayContainer() const = 0;
     /// is deletable
     virtual bool isDeletable() const { return false; /* usually not */ };
+    /// needs preparation before accessing
+    virtual bool needsPreparation(PropertyAccessMode aMode) const { return false; /* usually not */ };
     /// will be shown in wildcard queries
     virtual bool isWildcardAddressable() const { return true; };
     /// acts as root of a C++ class hierarchy
@@ -133,6 +137,7 @@ namespace p44 {
     virtual intptr_t objectKey() const P44_OVERRIDE { return descP->objectKey; }
     virtual bool isArrayContainer() const P44_OVERRIDE { return descP->propertyType & propflag_container; };
     virtual bool isWildcardAddressable() const P44_OVERRIDE { return (descP->propertyType & propflag_nowildcard)==0; };
+    virtual bool needsPreparation(PropertyAccessMode aMode) const P44_OVERRIDE { return (descP->propertyType & (aMode==access_read ? propflag_needsreadprep : propflag_needswriteprep))!=0; };
   };
 
 
@@ -144,7 +149,10 @@ namespace p44 {
   public:
     DynamicPropertyDescriptor(PropertyDescriptorPtr aParentDescriptor) :
       inherited(aParentDescriptor),
-      arrayContainer(false)
+      arrayContainer(false),
+      deletable(false),
+      needsReadPrep(false),
+      needsWritePrep(false)
     {};
     string propertyName; ///< name of the property
     ApiValueType propertyType; ///< type of the property value
@@ -152,6 +160,8 @@ namespace p44 {
     intptr_t propertyObjectKey; ///< identifier for container
     bool arrayContainer;
     bool deletable;
+    bool needsReadPrep;
+    bool needsWritePrep;
 
     virtual const char *name() const P44_OVERRIDE { return propertyName.c_str(); }
     virtual ApiValueType type() const P44_OVERRIDE { return propertyType; }
@@ -159,12 +169,30 @@ namespace p44 {
     virtual intptr_t objectKey() const P44_OVERRIDE { return propertyObjectKey; }
     virtual bool isArrayContainer() const P44_OVERRIDE { return arrayContainer; };
     virtual bool isDeletable() const P44_OVERRIDE { return deletable; };
+    virtual bool needsPreparation(PropertyAccessMode aMode) const P44_OVERRIDE { return aMode==access_read ? needsReadPrep : needsWritePrep; };
   };
 
 
 
 
   typedef boost::intrusive_ptr<PropertyContainer> PropertyContainerPtr;
+
+  typedef boost::function<void (ApiValuePtr aResultObject, ErrorPtr aError)> PropertyAccessCB;
+
+  class PropertyPrep
+  {
+    friend class PropertyContainer;
+
+    PropertyContainerPtr target;
+    PropertyDescriptorPtr propertyDescriptor;
+
+    PropertyPrep(PropertyContainerPtr aTarget, PropertyDescriptorPtr aDescriptor) : target(aTarget), propertyDescriptor(aDescriptor) {};
+  };
+
+  typedef list<PropertyPrep> PropertyPrepList;
+  typedef boost::shared_ptr<PropertyPrepList> PropertyPrepListPtr;
+
+
 
   /// Base class for objects providing API properties
   /// Implements generic mechanisms to handle accessing elements and subtrees of named propeties.
@@ -183,11 +211,11 @@ namespace p44 {
     /// read or write property
     /// @param aMode access mode (see PropertyAccessMode: read, write or write preload)
     /// @param aQueryObject the object defining the read or write query
-    /// @param aResultObject for read, must be an object
     /// @param aParentDescriptor the descriptor of the parent property, can be passed NULL at root level
     ///   (but will internally be repaced by a RootPropertyDescriptor)
-    /// @return Error 501 if property is unknown, 403 if property exists but cannot be accessed, 415 if value type is incompatible with the property
-    ErrorPtr accessProperty(PropertyAccessMode aMode, ApiValuePtr aQueryObject, ApiValuePtr aResultObject, int aDomain, PropertyDescriptorPtr aParentDescriptor);
+    /// @param aAccessCompleteCB will be called when property access is complete. Callback's aError
+    ///   returns Error 501 if property is unknown, 403 if property exists but cannot be accessed, 415 if value type is incompatible with the property
+    void accessProperty(PropertyAccessMode aMode, ApiValuePtr aQueryObject, int aDomain, PropertyDescriptorPtr aParentDescriptor, PropertyAccessCB aAccessCompleteCB);
 
     /// @}
 
@@ -244,6 +272,12 @@ namespace p44 {
     /// @note base class always returns NULL, which means no structured or proxy properties
     virtual PropertyContainerPtr getContainer(const PropertyDescriptorPtr &aPropertyDescriptor, int &aDomain) { return NULL; };
 
+    /// prepare access to a property (for example if the property needs I/O to update its value before being read).
+    /// @param aMode access mode (see PropertyAccessMode: read, write, write preload or delete)
+    /// @param aPropertyDescriptor decriptor that signalled a need for preparation
+    /// @note this base class just calls aPreparedCB with success status.
+    virtual void prepareAccess(PropertyAccessMode aMode, PropertyDescriptorPtr aPropertyDescriptor, StatusCB aPreparedCB);
+
     /// access single field in this container
     /// @param aMode access mode (see PropertyAccessMode: read, write, write preload or delete)
     /// @param aPropValue JsonObject with a single value
@@ -251,6 +285,7 @@ namespace p44 {
     /// @return false if value could not be accessed
     /// @note this base class always returns false, as it does not have any properties implemented
     virtual bool accessField(PropertyAccessMode aMode, ApiValuePtr aPropValue, PropertyDescriptorPtr aPropertyDescriptor) { return false; };
+
 
     /// post-process written properties in subcontainers. This is called after a property write access has
     /// compleded successfully in a subcontainer (as returned by this object's getContainer()), and can be used to commit container
@@ -265,6 +300,18 @@ namespace p44 {
 
     /// @name utility methods
     /// @{
+
+    /// internally read or write property
+    /// @param aMode access mode (see PropertyAccessMode: read, write or write preload)
+    /// @param aQueryObject the object defining the read or write query
+    /// @param aResultObject for read, must be an object
+    /// @param aParentDescriptor the descriptor of the parent property, can be passed NULL at root level
+    ///   (but will internally be repaced by a RootPropertyDescriptor)
+    /// @param aPreparationList if not NULL, this list will be filled with property descriptors that need preparation before accessing.
+    ///   Otherwise, properties are assumed to be prepared already and will be accessed directly
+    /// @return Error 501 if property is unknown, 403 if property exists but cannot be accessed, 415 if value type is incompatible with the property
+    ErrorPtr accessPropertyInternal(PropertyAccessMode aMode, ApiValuePtr aQueryObject, ApiValuePtr aResultObject, int aDomain, PropertyDescriptorPtr aParentDescriptor, PropertyPrepListPtr aPreparationList);
+
 
     /// parse aPropmatch for numeric index (both plain number and #n are allowed, plus empty and "*" wildcards)
     /// @param aPropMatch property name to match
@@ -298,6 +345,12 @@ namespace p44 {
     );
 
     /// @}
+
+  private:
+
+    void prepareNext(PropertyPrepListPtr aPrepList, PropertyAccessMode aMode, ApiValuePtr aQueryObject, int aDomain, PropertyDescriptorPtr aParentDescriptor, PropertyAccessCB aAccessCompleteCB, ErrorPtr aError);
+
+
 
   };
   

commit 938153787c33257955a2312013b09cd1b0727577

DsAddressable: event-only pushNotifications did not get sent any more.
(Regression caused by 2eaa012398 (Property access: added mechanism to allow a preparation step (possibly needing I/O) before a property can be accessed.))

vdc_common/dsaddressable.cpp
@@ -227,8 +227,6 @@ bool DsAddressable::pushNotification(ApiValuePtr aPropertyQuery, ApiValuePtr aEv
 {
   if (announced!=Never) {
     // device is announced: push can take place
-    ErrorPtr err;
-    ApiValuePtr value;
     if (aPropertyQuery) {
       // a property change is to be notified
       accessProperty(
@@ -236,7 +234,11 @@ bool DsAddressable::pushNotification(ApiValuePtr aPropertyQuery, ApiValuePtr aEv
         boost::bind(&DsAddressable::pushPropertyReady, this, aEvents, _1, _2)
       );
     }
-    return true; // although not yet sent, assume push will be ok
+    else {
+      // no property query, event-only -> send events right away
+      pushPropertyReady(aEvents, ApiValuePtr(), ErrorPtr());
+    }
+    return true; // although possibly (when we have properties to push) not yet sent now, assume push will be ok
   }
   else {
     if (isPublicDS()) {