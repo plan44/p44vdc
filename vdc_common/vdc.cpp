plan44/p44vdc

commit a475c69aa4f9cafbe2b184ec5766e9871e731d4b

vDC modelVersion: now vdc-specfic version information such as hue bridge or netatmo version can be added using vdcModelName()

vdc_common/vdc.cpp
@@ -256,6 +256,20 @@ string Vdc::vendorName()
 }
 
 
+string Vdc::modelVersion() const
+{
+  string v = vdcModelSuffix();
+  if (v.empty()) {
+    return inherited::modelVersion();
+  }
+  else {
+    return inherited::modelVersion() + " / " + v;
+  }
+}
+
+
+
+
 string Vdc::modelName()
 {
   // derive the descriptive name

deviceclasses/external/externalvdc.cpp
@@ -1493,6 +1493,9 @@ ErrorPtr ExternalDeviceConnector::handleDeviceApiJsonSubMessage(JsonObjectPtr aM
       if (aMessage->get("modelname", o)) {
         externalVdc.modelNameString = o->stringValue();
       }
+      if (aMessage->get("modelversion", o)) {
+        externalVdc.modelVersionString = o->stringValue();
+      }
       // - get icon base name
       if (aMessage->get("iconname", o)) {
         externalVdc.iconBaseName = o->stringValue();
@@ -1632,6 +1635,14 @@ string ExternalVdc::modelName()
     return modelNameString;
   return inherited::modelName();
 }
+
+
+string ExternalVdc::vdcModelVersion() const
+{
+  return modelVersionString;
+};
+
+
 
 
 bool ExternalVdc::getDeviceIcon(string &aIcon, bool aWithData, const char *aResolutionPrefix)

deviceclasses/external/externalvdc.hpp
@@ -331,6 +331,7 @@ namespace p44 {
 
     string iconBaseName; ///< the base icon name
     string modelNameString; ///< the string to be returned by modelName()
+    string modelVersionString; ///< the string to be returned by vdcModelVersion()
     string configUrl; ///< custom value for configURL if not empty
     bool alwaysVisible; ///< visible even if no devices contained
 
@@ -361,8 +362,14 @@ namespace p44 {
     /// @{
 
     /// @return human readable, language independent model name/short description
-    /// @note base class will construct this from global product name and vdcModelSuffix()
+    /// @note when no specific modelNameString is set via external API,
+    ///   base class will construct this from global product name and vdcModelSuffix()
     virtual string modelName() P44_OVERRIDE;
+
+    /// @return human readable model version specific to that vDC, meaning for example a firmware version
+    ///    of external hardware governing the access to a device bus/network such as a hue bridge.
+    ///    If not empty, this will be appended to the modelVersion() string.
+    virtual string vdcModelVersion() const P44_OVERRIDE;
 
     /// @return URL for Web-UI (for access from local LAN)
     virtual string webuiURLString() P44_OVERRIDE;

deviceclasses/hue/huedevice.cpp
@@ -203,7 +203,7 @@ string HueDevice::hardwareGUID()
     return inherited::hardwareGUID();
 }
 
-string HueDevice::modelVersion()
+string HueDevice::modelVersion() const
 {
   return swVersion;
 }

deviceclasses/hue/huedevice.hpp
@@ -137,7 +137,7 @@ namespace p44 {
     virtual string hardwareGUID() P44_OVERRIDE;
 
     /// @return human readable version string
-    virtual string modelVersion() P44_OVERRIDE;
+    virtual string modelVersion() const P44_OVERRIDE;
 
     /// Get icon data or name
     /// @param aIcon string to put result into (when method returns true)

deviceclasses/hue/huevdc.hpp
@@ -108,6 +108,9 @@ namespace p44 {
     ///   Will be appended to product name to create modelName() for vdcs
     virtual string vdcModelSuffix() const P44_OVERRIDE { return "hue"; }
 
+    /// @return human readable model version specific to that vDC
+    virtual string vdcModelVersion() const P44_OVERRIDE { return swVersion; };
+
     /// @return hardware GUID in URN format to identify hardware as uniquely as possible
     /// - uuid:UUUUUUU = UUID
     /// - http:xxxx = API

vdc_common/dsaddressable.cpp
@@ -49,7 +49,7 @@ DsAddressable::~DsAddressable()
 }
 
 
-string DsAddressable::modelVersion()
+string DsAddressable::modelVersion() const
 {
   // Note: it is important to override this at vdchost level, because would loop otherwise
   return getVdcHost().modelVersion();

vdc_common/dsaddressable.hpp
@@ -199,9 +199,12 @@ namespace p44 {
     /// @return human readable, language independent model name/short description
     virtual string modelName() = 0;
 
-    /// @return human readable version string
-    /// @note base class implementation returns version string of vdc host by default
-    virtual string modelVersion();
+    /// @return human readable version string of the device or vDC model
+    /// @note base class implementation returns version string of vdc host by default.
+    ///   Derived device classes should return version information for the actual device,
+    ///   if available from device hardware.
+    ///   Do not derive modelVersion() in vDCs, but override Vdc::vdcModelVersion() instead.
+    virtual string modelVersion() const;
 
     /// @return human readable display Id (such as serial number or similar, device instance identifying string)
     /// @note this is intended as a way to show the user the hardwareGUID in a more readable, possibly beautified manner.

commit 121fd2f8d126a9a31f51000e95ee72299e736aae

vdc: fixed modelVersion, did include model suffix instead of version
This was broken in:
a475c69aa4 (vDC modelVersion: now vdc-specfic version information such as hue bridge or netatmo version can be added using vdcModelName())

vdc_common/vdc.cpp
@@ -258,7 +258,7 @@ string Vdc::vendorName()
 
 string Vdc::modelVersion() const
 {
-  string v = vdcModelSuffix();
+  string v = vdcModelVersion();
   if (v.empty()) {
     return inherited::modelVersion();
   }