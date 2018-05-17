a06b5d31ccdd4c6a84fcfb5243b7f7def4506390
Lukas Zeller <luz@plan44.ch>
2018-05-17 14:49:55 +0200

    Sensors: rrd logging: prepare logging early, after loading settings. Log pushed value in a way showing the state upstream currently knows of.

--- behaviours/sensorbehaviour.cpp
+++ behaviours/sensorbehaviour.cpp
@@ -609,7 +609,7 @@ static string rrdminmax(double aMin, double aMax)
 }
 
 
-void SensorBehaviour::logSensorValue(MLMicroSeconds aTimeStamp, double aRawValue, double aProcessedValue, double aPushedValue)
+void SensorBehaviour::prepareLogging()
 {
   if (!loggingReady && !rrdbconfig.empty() && rrdbfile.empty()) {
     // configured but not yet prepared to log
@@ -731,7 +731,7 @@ void SensorBehaviour::logSensorValue(MLMicroSeconds aTimeStamp, double aRawValue
       if (ret==0) {
         BLOG(LOG_INFO, "rrd: successfully created new rrd file '%s'", rrdbfile.c_str());
         loggingReady = true;
-        lastRrdUpdate = aTimeStamp; // creation counts as update, make sure we don't immediately try to send first update afterwards
+        lastRrdUpdate = MainLoop::currentMainLoop().now(); // creation counts as update, make sure we don't immediately try to send first update afterwards
       }
       else {
         BLOG(LOG_ERR, "rrd: cannot create rrd file '%s': %s", rrdbfile.c_str(), rrd_get_error());
@@ -743,6 +743,13 @@ void SensorBehaviour::logSensorValue(MLMicroSeconds aTimeStamp, double aRawValue
       loggingReady = true;
     }
   }
+}
+
+
+void SensorBehaviour::logSensorValue(MLMicroSeconds aTimeStamp, double aRawValue, double aProcessedValue, double aPushedValue)
+{
+  // make sure logging is prepared
+  prepareLogging();
   // now actually log into file
   if (loggingReady && lastRrdUpdate<aTimeStamp-10*Second) {
     lastRrdUpdate = aTimeStamp;
@@ -760,9 +767,9 @@ void SensorBehaviour::logSensorValue(MLMicroSeconds aTimeStamp, double aRawValue
     // - filtered (considered unknown when sensor is invalid)
     i = ud.find("%F");
     if (i!=string::npos) ud.replace(i, 2, rrdval(aProcessedValue, lastUpdate!=Never).c_str());
-    // - filtered (considered unknown when pushed within aliveSignInterval)
+    // - pushed (considered unknown when never pushed, or last state pushed must have been NULL because of invalidation)
     i = ud.find("%P");
-    if (i!=string::npos) ud.replace(i, 2, rrdval(aPushedValue, lastPush!=Never && lastPush+aliveSignInterval>lastUpdate).c_str());
+    if (i!=string::npos) ud.replace(i, 2, rrdval(aPushedValue, lastPush!=Never && lastUpdate!=Never).c_str());
     args.push_back(ud);
     int ret = rrd_call(rrd_update, args);
     if (ret!=0) {
@@ -832,6 +839,8 @@ void SensorBehaviour::loadFromRow(sqlite3pp::query::iterator &aRow, int &aIndex,
   #if ENABLE_RRDB
   aRow->getIfNotNull(aIndex++, rrdbconfig);
   aRow->getIfNotNull(aIndex++, rrdbpath);
+  // make sure logging is ready (if enabled at all)
+  prepareLogging();
   #endif
 }
 
