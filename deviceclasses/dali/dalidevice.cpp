plan44/p44vdc

commit 94c225dbc7a7b784d081125353299352d350ac90

Device/DALI: optimized scene calls: device must have a chance to sync its output value caches after a indirectly applied output change (such as native scene/group) call.

deviceclasses/dali/dalidevice.cpp
@@ -1448,7 +1448,7 @@ void DaliSingleControllerDevice::dimChannel(ChannelBehaviourPtr aChannel, VdcDim
       // in all cases, we need to query current brightness after dimming
       if (aDimMode==dimmode_stop) {
         // retrieve end status
-        daliController->updateParams(boost::bind(&DaliSingleControllerDevice::dimEndStateRetrieved, this, _1));
+        daliController->updateParams(boost::bind(&DaliSingleControllerDevice::outputChangeEndStateRetrieved, this, _1));
       }
     }
     else {
@@ -1459,7 +1459,18 @@ void DaliSingleControllerDevice::dimChannel(ChannelBehaviourPtr aChannel, VdcDim
 }
 
 
-void DaliSingleControllerDevice::dimEndStateRetrieved(ErrorPtr aError)
+void DaliSingleControllerDevice::sceneValuesApplied(SimpleCB aDoneCB, DsScenePtr aScene, bool aIndirectly)
+{
+  if (aIndirectly) {
+    // some values were applied indirectly (e.g. optimized group/scene), need to read back from hardware into
+    // Note: delay to make sure operation has completed (usually)
+    outputSyncTicket.executeOnce(boost::bind(&DaliBusDevice::updateParams, daliController, StatusCB()), 3*Second);
+  }
+  inherited::sceneValuesApplied(aDoneCB, aScene, aIndirectly);
+}
+
+
+void DaliSingleControllerDevice::outputChangeEndStateRetrieved(ErrorPtr aError)
 {
   processUpdatedParams(aError);
 }
@@ -1651,7 +1662,6 @@ int DaliCompositeDevice::opStateLevel()
 
 string DaliCompositeDevice::getOpStateText()
 {
-  bool missing = false;
   bool failure = false;
   bool incomplete = false;
   bool noUniqueId = false;

deviceclasses/dali/dalidevice.hpp
@@ -321,6 +321,7 @@ namespace p44 {
   protected:
 
     MLTicket transitionTicket; ///< transition timing ticket
+    MLTicket outputSyncTicket; ///< output value sync-back ticket
 
   public:
 
@@ -435,6 +436,15 @@ namespace p44 {
     ///   class makes sure these cases (which may occur at the vDC API level) are not passed on to dimChannel()
     virtual void dimChannel(ChannelBehaviourPtr aChannel, VdcDimMode aDimMode, bool aDoApply) P44_OVERRIDE;
 
+    /// is called when scene values are applied, either via applyChannelValues or via optimized calls
+    /// @param aDoneCB called when all tasks following applying the scene are done
+    /// @param aScene the scene that was called
+    /// @param aIndirectly if true, applyChannelValues was NOT used to apply the scene, but STILL some other mechanism
+    ///   such as optimized group call has changed outputs. device implementation might need to sync back hardware state in this case.
+    /// @note aIndirectly is NOT set when there was no output change at all and applyChannelValues() was therefore not called.
+    /// @note if derived in subclass, base class' implementation should normally be called as this triggers scene actions
+    virtual void sceneValuesApplied(SimpleCB aDoneCB, DsScenePtr aScene, bool aIndirectly) P44_FINAL;
+
     /// @}
 
     /// @name identification of the addressable entity
@@ -513,7 +523,7 @@ namespace p44 {
   private:
 
     void processUpdatedParams(ErrorPtr aError);
-    void dimEndStateRetrieved(ErrorPtr aError);
+    void outputChangeEndStateRetrieved(ErrorPtr aError);
     void daliControllerSynced(StatusCB aCompletedCB, bool aFactoryReset, ErrorPtr aError);
     void checkPresenceResponse(PresenceCB aPresenceResultHandler);
     void disconnectableHandler(bool aForgetParams, DisconnectCB aDisconnectResultHandler, bool aPresent);

vdc_common/device.cpp
@@ -1612,22 +1612,22 @@ void Device::callSceneExecutePrepared(SimpleCB aDoneCB, NotificationType aWhatTo
         // now we can apply values to hardware
         if (aWhatToApply!=ntfy_none) {
           // normally apply channel values to hardware
-          requestApplyingChannels(boost::bind(&Device::sceneValuesApplied, this, aDoneCB, scene), false);
+          requestApplyingChannels(boost::bind(&Device::sceneValuesApplied, this, aDoneCB, scene, false), false);
           return;
         }
         else {
           // just consider all channels already applied (e.g. by vdc-level native action)
           // - confirm having applied channels (normally, actual device-level apply would do that)
           allChannelsApplied();
-          // - consider scene applied
-          sceneValuesApplied(aDoneCB, scene);
+          // - consider scene applied but indirectly
+          sceneValuesApplied(aDoneCB, scene, true);
           return;
         }
       }
     }
     else {
       // no apply to channels or hardware needed, directly proceed to actions
-      sceneValuesApplied(aDoneCB, scene);
+      sceneValuesApplied(aDoneCB, scene, false);
       return;
     }
   }
@@ -1636,7 +1636,7 @@ void Device::callSceneExecutePrepared(SimpleCB aDoneCB, NotificationType aWhatTo
 }
 
 
-void Device::sceneValuesApplied(SimpleCB aDoneCB, DsScenePtr aScene)
+void Device::sceneValuesApplied(SimpleCB aDoneCB, DsScenePtr aScene, bool aIndirectly)
 {
   // now perform scene special actions such as blinking
   performSceneActions(aScene, boost::bind(&Device::sceneActionsComplete, this, aDoneCB, aScene));

vdc_common/device.hpp
@@ -612,6 +612,15 @@ namespace p44 {
     ///   Device::requestApplyingChannels() provides an implementation that serializes calls to applyChannelValues and syncChannelValues
     virtual void applyChannelValues(SimpleCB aDoneCB, bool aForDimming) { if (aDoneCB) aDoneCB(); /* just call completed in base class */ };
 
+    /// is called when scene values are applied, either via applyChannelValues or via optimized calls
+    /// @param aDoneCB called when all tasks following applying the scene are done
+    /// @param aScene the scene that was called
+    /// @param aIndirectly if true, applyChannelValues was NOT used to apply the scene, but STILL some other mechanism
+    ///   such as optimized group call has changed outputs. device implementation might need to sync back hardware state in this case.
+    /// @note aIndirectly is NOT set when there was no output change at all and applyChannelValues() was therefore not called.
+    /// @note if derived in subclass, base class' implementation should normally be called as this triggers scene actions
+    virtual void sceneValuesApplied(SimpleCB aDoneCB, DsScenePtr aScene, bool aIndirectly);
+
     /// synchronize channel values by reading them back from the device's hardware (if possible)
     /// @param aDoneCB will be called when values are updated with actual hardware values
     /// @note this method is only called at startup and before saving scenes to make sure changes done to the outputs directly (e.g. using
@@ -726,7 +735,6 @@ namespace p44 {
     void dimHandler(ChannelBehaviourPtr aChannel, double aIncrement, MLMicroSeconds aNow);
     void dimDoneHandler(ChannelBehaviourPtr aChannel, double aIncrement, MLMicroSeconds aNextDimAt);
     void outputSceneValueSaved(DsScenePtr aScene);
-    void sceneValuesApplied(SimpleCB aDoneCB, DsScenePtr aScene);
 
     void applyingChannelsComplete();
     void updatingChannelsComplete();

commit 7f58d644f3515e9972091e7bd060109240d3e39f

DALI: fix crash introduced with 94c225dbc7, updateParams() was not safe to be invoked with NULL callback

deviceclasses/dali/dalidevice.cpp
@@ -461,7 +461,7 @@ void DaliBusDevice::initializeFeatures(StatusCB aCompletedCB)
 void DaliBusDevice::updateParams(StatusCB aCompletedCB)
 {
   if (isDummy) {
-    aCompletedCB(ErrorPtr());
+    if (aCompletedCB) aCompletedCB(ErrorPtr());
     return;
   }
   // query actual arc power level
@@ -510,7 +510,7 @@ void DaliBusDevice::queryMinLevelResponse(StatusCB aCompletedCB, bool aNoOrTimeo
     );
     return;
   }
-  aCompletedCB(aError);
+  if (aCompletedCB) aCompletedCB(aError);
 }
 
 
@@ -569,7 +569,7 @@ void DaliBusDevice::queryColorStatusResponse(StatusCB aCompletedCB, bool aNoOrTi
     }
   }
   // no more queries
-  aCompletedCB(aError);
+  if (aCompletedCB) aCompletedCB(aError);
 }
 
 
@@ -590,7 +590,7 @@ void DaliBusDevice::queryXCoordResponse(StatusCB aCompletedCB, uint16_t aRespons
       return;
     }
   }
-  aCompletedCB(aError);
+  if (aCompletedCB) aCompletedCB(aError);
 }
 
 
@@ -600,7 +600,7 @@ void DaliBusDevice::queryYCoordResponse(StatusCB aCompletedCB, uint16_t aRespons
     currentY = aResponse16;
     LOG(LOG_INFO, "DaliBusDevice: DT8 - is in CIE X/Y color mode, X=%.3f, Y=%.3f", (double)currentXorCT/65536, (double)currentY/65536);
   }
-  aCompletedCB(aError);
+  if (aCompletedCB) aCompletedCB(aError);
 }
 
 
@@ -615,7 +615,7 @@ void DaliBusDevice::queryCTResponse(StatusCB aCompletedCB, uint16_t aResponse16,
       LOG(LOG_INFO, "DaliBusDevice: DT8 - is in Tunable White mode, CT=%hd mired", currentXorCT);
     }
   }
-  aCompletedCB(aError);
+  if (aCompletedCB) aCompletedCB(aError);
 }
 
 
@@ -649,7 +649,7 @@ void DaliBusDevice::queryRGBWAFResponse(StatusCB aCompletedCB, uint16_t aResInde
     );
     return;
   }
-  aCompletedCB(aError);
+  if (aCompletedCB) aCompletedCB(aError);
 }
 
 

deviceclasses/dali/dalidevice.hpp
@@ -79,13 +79,15 @@ namespace p44 {
     bool isPresent; ///< set if present
     bool lampFailure; ///< set if lamp has failure
 
-    /// cached parameters (call updateParams() to update these)
-    Brightness currentBrightness; ///< current brightness
-    Brightness minBrightness; ///< currently set minimal brightness
+    /// cached transition parameters (updated in setTransitionTime() and dimPrepare())
     MLMicroSeconds currentTransitionTime; ///< currently set transition time
     uint8_t currentFadeTime; ///< currently set DALI fade time
     double currentDimPerMS; ///< current dim steps per second
     uint8_t currentFadeRate; ///< currently set DALI fade rate
+
+    /// cached parameters (call updateParams() to update these)
+    Brightness currentBrightness; ///< current brightness
+    Brightness minBrightness; ///< currently set minimal brightness
     // - DT8 params
     ColorLightMode currentColorMode; ///< current color mode
     uint16_t currentXorCT; ///< current CIE X or CT or Red