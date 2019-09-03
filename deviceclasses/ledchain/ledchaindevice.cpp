plan44/p44vdc

commit 1b2731def78f46df46b0ed38a9027b93a78e70f1

LedChainDevice: views must be black by default

deviceclasses/ledchain/ledchaindevice.cpp
@@ -65,6 +65,8 @@ public:
     startSoftEdge(aStartSoftEdge),
     endSoftEdge(aEndSoftEdge)
   {
+    setForegroundColor(black);
+    setBackgroundColor(black);
     PixelRect f = { aX, aY, aDx, aDy };
     setFrame(f);
     setFullFrameContent();
@@ -82,6 +84,8 @@ public:
 
   FeatureLightSpot(int aX, int aDx, int aY, int aDy)
   {
+    setForegroundColor(black);
+    setBackgroundColor(black);
     PixelRect f = { aX, aY, aDx, aDy };
     setFrame(f);
     setFullFrameContent();

commit 7078bb1863b7431b89c9511896d5f6d372e858a1

LedChainDevice: view background (which shines through when foreground is fully transparent) must not be black, but also transparent.
This corrects a regression when overlaying lights from "1b2731def7 (LedChainDevice: views must be black by default)"

deviceclasses/ledchain/ledchaindevice.cpp
@@ -66,7 +66,7 @@ public:
     endSoftEdge(aEndSoftEdge)
   {
     setForegroundColor(black);
-    setBackgroundColor(black);
+    setBackgroundColor(transparent);
     PixelRect f = { aX, aY, aDx, aDy };
     setFrame(f);
     setFullFrameContent();
@@ -85,7 +85,7 @@ public:
   FeatureLightSpot(int aX, int aDx, int aY, int aDy)
   {
     setForegroundColor(black);
-    setBackgroundColor(black);
+    setBackgroundColor(transparent);
     PixelRect f = { aX, aY, aDx, aDy };
     setFrame(f);
     setFullFrameContent();