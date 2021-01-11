plan44/p44vdc

commit 395aa55b62bb15b3481de15f008c8052b395486c

DALI: add better description for composite lights (list all channels)

deviceclasses/dali/dalidevice.cpp
@@ -1065,7 +1065,7 @@ string DaliBusDeviceGroup::description()
     if (!g.empty()) g +=", ";
     string_format_append(g, "%02d", *pos);
   }
-  string s = "\n- DALI group - device bus addresses: " + g;
+  string s = string_format("\n- DALI group #%d - device bus addresses: %s", deviceInfo->shortAddress & DaliGroupMask, g.c_str());
   s + inherited::description();
   return s;
 }
@@ -2118,8 +2118,18 @@ string DaliCompositeDevice::oemModelGUID()
 string DaliCompositeDevice::description()
 {
   string s = inherited::description();
-  DaliBusDevicePtr dimmer = firstBusDevice();
-  if (dimmer) s.append(dimmer->description());
+  for (DimmerIndex idx=dimmer_red; idx<numDimmers; idx++) {
+    DaliBusDevicePtr dim = dimmers[idx];
+    s.append("\nChannel %s", dimmerChannelNames[idx]);
+    if (dim) {
+      // add busdevice info
+      s.append(dim->description());
+    }
+    else {
+      // missing bus device
+      s.append("\n- missing dimmer");
+    }
+  }
   return s;
 }
 

commit 86268524f4254f62e62a4caa05aa0bc9b88defa5

dalidevice: fix crash rendering description of composite devices (regression introduced in 395aa55)

deviceclasses/dali/dalidevice.cpp
@@ -80,7 +80,7 @@ DaliBusDevice::DaliBusDevice(DaliVdc &aDaliVdc) :
 
 string DaliBusDevice::logContextPrefix()
 {
-  return string_format("DaliBusDevice %02d: ", deviceInfo ? deviceInfo->shortAddress : 99);
+  return string_format("DaliBusDevice %02d", deviceInfo ? deviceInfo->shortAddress : 99);
 }
 
 
@@ -2120,7 +2120,7 @@ string DaliCompositeDevice::description()
   string s = inherited::description();
   for (DimmerIndex idx=dimmer_red; idx<numDimmers; idx++) {
     DaliBusDevicePtr dim = dimmers[idx];
-    s.append("\nChannel %s", dimmerChannelNames[idx]);
+    string_format_append(s, "\nChannel %s", dimmerChannelNames[idx]);
     if (dim) {
       // add busdevice info
       s.append(dim->description());