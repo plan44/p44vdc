b7baa8598f66c92c5985f801440ae829332a5a23
Lukas Zeller <luz@plan44.ch>
2021-06-08 18:11:41 +0200

    apivalue: make sure structured objects which are not native JSON are still rendered as JSON in API values

--- vdc_common/apivalue.cpp
+++ vdc_common/apivalue.cpp
@@ -342,7 +342,8 @@ ApiValuePtr ApiValue::newScriptValue(P44Script::ScriptObjPtr aValue)
   if (!aValue || aValue->undefined()) return newNull();
   else if (aValue->hasType(P44Script::text)) return newString(aValue->stringValue());
   #if SCRIPTING_JSON_SUPPORT
-  else if (aValue->hasType(P44Script::json)) {
+  else if (aValue->hasType(P44Script::structured|P44Script::json)) {
+    // render native JSON as well as any other objects/arrays as JSON
     ApiValuePtr j = newNull();
     *j = *JsonApiValue::newValueFromJson(aValue->jsonValue());
     return j;
