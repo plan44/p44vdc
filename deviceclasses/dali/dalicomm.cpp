plan44/p44vdc

commit 4f09137c97278e7e5712ce7e34390dc5d3667fe8

dali: found and fixed problem with new dali shortaddress appearing twice in the list of used addrs
- this was the reason for the problem worked around in: "b72b2fd - DALI: avoid false duplicate detection when same shortaddress is in the list more than once"
- problem was that newShortAddress() did already push the new shortaddr, but later changes decided to do that only when programming was actually successful.
- keeping safeguard to preven false invalidating when duplicates still should appear, because that is really bad

deviceclasses/dali/dalicomm.cpp
@@ -1056,7 +1056,7 @@ private:
   }
 
 
-  // get new unused short address
+  // get new unused short address (but does NOT yet add it to usedShortAddr!
   DaliAddress newShortAddress()
   {
     DaliAddress newAddr = DALI_MAXDEVICES;
@@ -1064,7 +1064,6 @@ private:
       newAddr--;
       if (!isShortAddressInList(newAddr,usedShortAddrsPtr) && !isShortAddressInList(newAddr, conflictedShortAddrsPtr)) {
         // this one is free, use it
-        usedShortAddrsPtr->push_back(newAddr);
         return newAddr;
       }
     }
@@ -1271,11 +1270,9 @@ private:
     // store short address if real address
     // (if broadcast, means that this device is w/o short address because >64 devices are on the bus, or short address could not be programmed)
     if (aShortAddress!=NoDaliAddress) {
-      if (isShortAddressInList(aShortAddress, foundDevicesPtr)) {
-        // this should not happen, but maybe it can (we had a weird support case where logs indicated that)
-        SOLOG(daliComm, LOG_ERR, "internal inconsistency: short address #%d found again in list", aShortAddress);
-      }
-      else {
+      if (!isShortAddressInList(aShortAddress, foundDevicesPtr)) {
+        // prevent registering the same address twice (did happen before fixing bug with newShortAddress() 2024-06-27)
+        SOLOG(daliComm, LOG_NOTICE, "- new short address %d programming verified", aShortAddress);
         foundDevicesPtr->push_back(aShortAddress);
       }
     }

deviceclasses/dali/dalivdc.cpp
@@ -359,7 +359,7 @@ void DaliVdc::queryNextDev(DaliBusDeviceListPtr aBusDevices, DaliBusDeviceList::
           }
           if (thisDsuid==otherDsuid) {
             if ((*busdevpos)->mDeviceInfo->mShortAddress==(*refpos)->mDeviceInfo->mShortAddress) {
-              // should not happen, but apparently can for some not-yet-known reason, so log it here but prevent invalidating serial
+              // should not happen (and should not any more since we found the origin 2024-06-27), but keep safeguard anyway, as false invalidating is REALLY bad
               LOG(LOG_ERR, "internal inconsistentcy: same dSUID, same shortaddress %d: iterators %s", (*busdevpos)->mDeviceInfo->mShortAddress, refpos==busdevpos ? "same" : "DIFFERENT!");
             }
             else {

commit 0f7caab8f85d7954c1704082ce5435f2a9f43949

DALI: fix bad regression breaking short address assignments introduced in "4f09137 - dali: found and fixed..."
- bug caused assigning more than one new short address fail, because newly
  assigned addresses did not get reserved (added to mUsedShortAddrsPtr list),
  so next device w/o short address was programmed the same address again -> fail.

deviceclasses/dali/dalicomm.cpp
@@ -1061,14 +1061,17 @@ private:
   }
 
 
-  // get new unused short address (but does NOT yet add it to usedShortAddr!
+  /// get new unused short address (but does NOT yet add it to any address list)
+  /// Note: do not forget to EXPLICITLY add newly programmed addresses to
+  ///   mUsedShortAddrsPtr and mFoundDevicesPtr) after a new address is actually
+  ///   programmed and verified
   DaliAddress newShortAddress()
   {
     DaliAddress newAddr = DALI_MAXDEVICES;
     while (newAddr>0) {
       newAddr--;
       if (!isShortAddressInList(newAddr,mUsedShortAddrsPtr) && !isShortAddressInList(newAddr, mConflictedShortAddrsPtr)) {
-        // this one is free, use it
+        // this one is free and COULD be used
         return newAddr;
       }
     }
@@ -1279,6 +1282,10 @@ private:
         // prevent registering the same address twice (did happen before fixing bug with newShortAddress() 2024-06-27)
         SOLOG(mDaliComm, LOG_NOTICE, "- new short address %d programming verified", aShortAddress);
         mFoundDevicesPtr->push_back(aShortAddress);
+      }
+      // now is definitely in use - must alse be in mUsedShortAddrs
+      if (!isShortAddressInList(aShortAddress, mUsedShortAddrsPtr)) {
+        mUsedShortAddrsPtr->push_back(aShortAddress);
       }
     }
     // withdraw this device from further searches